bad68ebd8cb42f830e080afc8c8498912688832d
waset <waset@foxmail.com>
2022-10-17 23:34:45 +0800

    build: :package: Use Vueuse

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,44 @@
 <script setup lang="ts">
 import { ref } from "vue";
+// 引入 pinia
 import { useStore } from "../store/userStore";
+// 引入 vueuse
+import { useMouse, useDark, useToggle } from "@vueuse/core";
+const isDark = useDark({
+  selector: "body", //class渲染的标签
+  valueDark: "dark", //暗黑class名字
+  valueLight: "light", //高亮class名字
+});
+const toggleDark = useToggle(isDark);
 
-const userstore = useStore();
-
+// 接收接口参数
 defineProps<{ msg: string }>();
-
+// 获取鼠标坐标
+const { x, y } = useMouse();
+// 实例化 pinia
+const userstore = useStore();
+// 获取用户信息
 userstore.getUserInfo();
-
+// 实例化数字
 const count = ref(0);
 </script>
 
 <template>
   <h1 class="text-red">{{ msg }}</h1>
-  <div class="flex w100% justify-center">
-    <div>123</div>
-    <button class="i-carbon-sun dark:i-carbon-moon" />
+  <div class="p-y-5 flex justify-center">
+    <button class="i-carbon-sun dark:i-carbon-moon" @click="toggleDark()" />
   </div>
-  <div class="card">
+  <div class="card light:bg-white dark:bg-dark">
     <div class="">
       <button type="button" @click="count++">count is {{ count }}</button>
     </div>
-    <p>当前登录用户：{{ userstore.username }}</p>
+    <p class="light:text-black">当前登录用户：{{ userstore.username }}</p>
+    <p class="light:text-black">当前 x 轴坐标：{{ x }}</p>
+    <p class="light:text-black">当前 y 轴坐标：{{ y }}</p>
     <div class="">
       <t-button @click="userstore.updateUser">切换用户</t-button>
     </div>
-    <p>
+    <p class="light:text-black">
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
